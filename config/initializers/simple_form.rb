6510be31eccee2599960239c61386058ed804e78
beitaz <beitaz@icloud.com>
2018-05-08 13:22:41 +0800

    修改类型页面,修改 simple_form 样式

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -54,8 +54,8 @@ SimpleForm.setup do |config|
     ## Inputs
     # b.use :input, class: 'input', error_class: 'is-invalid', valid_class: 'is-valid'
     b.use :label_input
-    # b.use :hint,  wrap_with: { tag: :span, class: :hint }
-    # b.use :error, wrap_with: { tag: :span, class: :error }
+    b.use :hint,  wrap_with: { tag: :span, class: :hint }
+    b.use :error, wrap_with: { tag: :span, class: :error }
 
     ## full_messages_for
     # If you want to display the full error message for the attribute, you can
@@ -113,7 +113,7 @@ SimpleForm.setup do |config|
   # config.label_text = lambda { |label, required, explicit_label| "#{required} #{label}" }
 
   # You can define the class to use on all labels. Default is nil.
-  # config.label_class = nil
+  config.label_class = 'control-label'
 
   # You can define the default class to be used on forms. Can be overriden
   # with `html: { :class }`. Defaulting to none.
